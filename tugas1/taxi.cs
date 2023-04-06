e7be80592cda4a23e7743fd78ffefdc316e9df2e
Victus <Victus@LAPTOP-PBR9K99D>
2023-04-06 20:29:59 +0700

    Add project files.

---
+++ tugas1/taxi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas1
+{
+    internal class taxi
+    {
+
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("DriverName : {0}", DriverName);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("On Duty : Yes");
+            }
+            if (OnDuty == false)
+            {
+                Console.WriteLine("OnDunty : No");
+            }
+            Console.WriteLine("Number of passnger : {0}, NumPassenger");
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sudah selesai menjemput penumpang", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} sudah selesai mengantar penumpang", DriverName);
+        }
+    }
+}
